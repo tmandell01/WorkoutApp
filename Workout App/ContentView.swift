31981a7303ba0c2e1f283be5d822a84c3978c12b
Thomas Mandell <tmandell01@gmail.com>
2025-11-26 21:09:52 -0500

    Initial Commit

---
+++ Workout App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout App
+//
+//  Created by Thomas Mandell on 11/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout App/Workout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AppApp.swift
+//  Workout App
+//
+//  Created by Thomas Mandell on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
